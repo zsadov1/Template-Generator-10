4fabb235f00d3154454d8015325cfb321b0d50c5
zsadov1 <zsadov1@gmail.com>
2020-01-27 18:51:18 -0500

    Card changes

--- app.js
+++ app.js
@@ -9,55 +9,56 @@ const fs = requires ("fs");
 
 function htmlManager (name , id , email , officeNumber) {
     return ` 
-    <div class="card bg-dark">
-        <div class="card-header">
-            <h2>${name}</h2>
-            <h3>${Title}</h3> 
+    <div class="card bg-light" style="width: 18rem;">
+        <div class="card-body">
+            <h2 class="card-title">${name}</h2>
+            <h3 class="card-title">${Title}</h3> 
         </div>
     </div>
-    <div class="card-body bg-dark">
-        <p>ID: ${id}</p>
-        <p>Email: ${email}</p>
-        <p>Office Number: ${officeNumber}</p>
+    <div class="card-body bg-light" style="width: 18rem;">
+        <p class="card-text">ID: ${id}</p>
+        <p class="card-text">Email: ${email}</p>
+        <p class="card-text">Office Number: ${officeNumber}</p>
     </div>  
     </div>
-     `
-};
+     `;
+}
 
 function htmlEngineer (name , id , github) {
     return ` 
-    <div class="card bg-dark">
-        <div class="card-header">
-            <h2>${name}</h2>
-            <h3>${Title}</h3> 
+    <div class="card bg-light" style="width: 18rem;">
+        <div class="card-body">
+            <h2 class="card-title">${name}</h2>
+            <h3 class="card-title">${Title}</h3> 
         </div>
     </div>
-    <div class="card-body bg-dark">
-        <p>ID: ${id}</p>
-        <p>Email: ${email}</p>
+    <div class="card-body bg-light" style="width: 18rem;">
+        <p class="card-text">ID: ${id}</p>
+        <p class="card-text">Email: ${email}</p>
         <p>Github: <a href="https://github.com/${github}" target = "_blank">${github}</a></p>
     </div>  
     </div>
-    `
-};
+    `;
+}
 
 function htmlIntern(name , id , school) {
     return ` 
-    <div class="card bg-dark">
-        <div class="card-header">
-            <h2>${name}</h2>
-            <h3>${Title}</h3> 
+    <div class="card bg-light" style="width: 18rem;">
+        <div class="card-body">
+            <h2 class="card-title">${name}</h2>
+            <h3 class="card-title">${Title}</h3> 
         </div>
     </div>
-    <div class="card-body bg-dark">
-        <p>ID: ${id}</p>
-        <p>Email: ${email}</p>
-        <p>School: ${school}</p>
+    <div class="card-body bg-light" style="width: 18rem;">
+        <p class="card-text">ID: ${id}</p>
+        <p class="card-text">Email: ${email}</p>
+        <p class="card-text">School: ${school}</p>
     </div>  
     </div>
-    `
-};
 
+    `;
+}
 
+function htmlMain 
 
 
